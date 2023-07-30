5eb186a1696f8c529b674c51133fc79e682e3081
Reece Mercer <reecemercer981@gmail.com>
2023-07-30 21:32:08 +0100

    #23 Stub unit test class for EditsViewModel

---
+++ NVRAMTuner.Test/ViewModels/Variables/EditsViewModelTests.cs
@@ -0,0 +1,14 @@
+﻿namespace NVRAMTuner.Test.ViewModels.Variables
+{
+    using Client.ViewModels.Variables;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Tests targeting <see cref="EditsViewModel"/>
+    /// </summary>
+    [TestClass]
+    public class EditsViewModelTests
+    {
+        
+    }
+}
