94ec50d0ae96b50a987d1d8f70f1080736ea3fa3
Reece Mercer <reecemercer981@gmail.com>
2023-07-31 21:07:33 +0100

    #25 added initial  wrapper class and interface

---
+++ NVRAMTuner.Client/Services/Wrappers/Interfaces/IMessengerService.cs
@@ -0,0 +1,15 @@
+﻿namespace NVRAMTuner.Client.Services.Wrappers.Interfaces
+{
+    using CommunityToolkit.Mvvm.Messaging;
+
+    /// <summary>
+    /// Interface for a service class wrapping around various <see cref="IMessenger"/> methods
+    /// </summary>
+    public interface IMessengerService
+    {
+        /// <summary>
+        /// <inheritdoc cref="IMessenger.Send{TMessage,TToken}"/>
+        /// </summary>
+        TMessage Send<TMessage>(TMessage message) where TMessage : class;
+    }
+}
---
+++ NVRAMTuner.Client/Services/Wrappers/MessengerService.cs
@@ -0,0 +1,33 @@
+﻿namespace NVRAMTuner.Client.Services.Wrappers
+{
+    using CommunityToolkit.Mvvm.Messaging;
+    using Interfaces;
+
+    /// <summary>
+    /// Service class aimed to wrap <see cref="IMessenger"/>
+    /// </summary>
+    public class MessengerService : IMessengerService
+    {
+        /// <summary>
+        /// An instance of <see cref="IMessenger"/>
+        /// </summary>
+        private readonly IMessenger messenger;
+
+        /// <summary>
+        /// Initialises a new instance of the <see cref="MessengerService"/> class
+        /// </summary>
+        /// <param name="messenger">An instance of <see cref="IMessenger"/></param>
+        public MessengerService(IMessenger messenger)
+        {
+            this.messenger = messenger;
+        }
+
+        /// <summary>
+        /// <inheritdoc cref="IMessenger.Send{TMessage,TToken}"/>
+        /// </summary>
+        public TMessage Send<TMessage>(TMessage message) where TMessage : class
+        {
+            return this.messenger.Send(message);
+        }
+    }
+}
